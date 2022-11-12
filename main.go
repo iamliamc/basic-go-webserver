47559ac70a1e9faa3fffa88a509eb0654be938be
Liam Considine <liam.considine@gusto.com>
2022-11-12 13:30:22 -0500

    Finish exploring constants

--- main.go
+++ main.go
@@ -2,7 +2,57 @@ package main
 
 import "fmt"
 
+const (
+	first  = 1
+	second = 2
+
+	firstIota  = iota + 6
+	secondIota = 2 << iota
+)
+
+// Iota resets between constant blocks
+const (
+	block = iota
+	evolveBlock
+	evolveBlock2
+)
+
+const (
+	// Iota 0 + 6 = 6
+	constantExpression = iota + 6
+	// Iota 1 + 6 = 7
+	repeatLastConstantExpression
+)
+
 func main() {
+	exploreConstantValues()
+	explorePointerValues()
+	exploreValueTypes()
+}
+
+func exploreConstantValues() {
+	fmt.Println("exploreConstantValues")
+	// implicit type
+	const c = 3
+	fmt.Println(c + 0.1415)
+	fmt.Println(first, second)
+
+	// One times two, five times
+	bigShiftExample := 1 << 5
+
+	// 32 divided by two, five times
+	downShiftExample := bigShiftExample >> 5
+	fmt.Println(bigShiftExample, downShiftExample)
+
+	// Iota examples
+	fmt.Println(firstIota, secondIota)
+	fmt.Println(block, evolveBlock, evolveBlock2)
+	fmt.Println(constantExpression, repeatLastConstantExpression)
+}
+
+func explorePointerValues() {
+	fmt.Println("explorePointerValues")
+
 	// pointer operator
 	var firstName *string = new(string)
 
@@ -21,7 +71,7 @@ func main() {
 }
 
 func exploreValueTypes() {
-	fmt.Println("Hello from a module, Gophers!")
+	fmt.Println("exploreValueTypes")
 	var i int
 	i = 42
 	fmt.Println(i)
