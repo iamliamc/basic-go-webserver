ffab4ea98820108b1544271815dd77b1a43d2801
Liam Considine <liam.considine@gusto.com>
2022-11-12 12:03:00 -0500

    WIP for pluralsight go getting-started

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from a module, Gophers!")
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.141516
+	fmt.Println(f)
+
+	firstName := "Liam"
+	fmt.Println(firstName)
+}
